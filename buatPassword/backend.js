ad65e6751b13599119f7f94805cd53ccb067b22c
Munif Shahliansyah <munf.syahli@gmail.com>
2024-05-28 11:11:09 +0700

    addFunction penjumlahan

--- buatPassword/backend.js
+++ buatPassword/backend.js
@@ -2,3 +2,11 @@ function userPass() {
   let pass = document.getElementById('inputPassword').value
   alert(pass)
 }
+
+function penjumlahan(a, b) {
+  let jumlah = a + b
+  console.log(jumlah) 
+}
+
+penjumlahan(3,4)
+
